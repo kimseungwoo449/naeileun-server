a2610a891f2ba930df122cc3a471e412138154ee
c-chae-eun-n <ce.hue125@gmail.com>
2024-05-21 17:26:14 +0900

    Update : ServiceServlet command 변수 request 변경

--- src/main/java/board/controller/ServiceServlet.java
+++ src/main/java/board/controller/ServiceServlet.java
@@ -11,7 +11,8 @@ public class ServiceServlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
 	
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		String command = request.getParameter("command"); // input type hidden
+		String command = request.getPathInfo();
+		System.out.println("command : " + command);
 		
 		if(command != null) {
 			ActionFactory af = ActionFactory.getInstance();
