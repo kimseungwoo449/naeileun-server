cafcf989c1f61e4f263648a89038a340fe65d3e9
c-chae-eun-n <ce.hue125@gmail.com>
2024-05-22 10:41:20 +0900

    Update : Board 변수 추가

--- src/main/java/board/model/Board.java
+++ src/main/java/board/model/Board.java
@@ -1,5 +1,19 @@
 package board.model;
 
+import java.sql.Timestamp;
+
 public class Board {
+	private int boardCode;
+	private String boardName;
+	private String description;
+	private Timestamp createDate;
+	private String title;
+	private String content;
+	private int userCode;
+	private Timestamp writeDate;
+	private Timestamp updateDate;
+	private int recommandation;
+	private int postCode;
+
 	
 }
