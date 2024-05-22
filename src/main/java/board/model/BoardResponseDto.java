478734a791283db9c236dd7d3d114491459fef2d
c-chae-eun-n <ce.hue125@gmail.com>
2024-05-22 10:56:30 +0900

    Update : 생성자 추가

--- src/main/java/board/model/BoardResponseDto.java
+++ src/main/java/board/model/BoardResponseDto.java
@@ -14,5 +14,37 @@ public class BoardResponseDto {
 	private Timestamp updateDate;
 	private int recommandation;
 	private int postCode;
+	
+	public BoardResponseDto(int boardCode, String boardName, String description, Timestamp createDate, String title,
+			String content, int userCode, Timestamp writeDate, Timestamp updateDate, int recommandation, int postCode) {
+		super();
+		this.boardCode = boardCode;
+		this.boardName = boardName;
+		this.description = description;
+		this.createDate = createDate;
+		this.title = title;
+		this.content = content;
+		this.userCode = userCode;
+		this.writeDate = writeDate;
+		this.updateDate = updateDate;
+		this.recommandation = recommandation;
+		this.postCode = postCode;
+	}
 
+	public BoardResponseDto(int boardCode, String boardName, Timestamp createDate, String title, String content,
+			int userCode, Timestamp writeDate, Timestamp updateDate, int recommandation, int postCode) {
+		super();
+		this.boardCode = boardCode;
+		this.boardName = boardName;
+		this.createDate = createDate;
+		this.title = title;
+		this.content = content;
+		this.userCode = userCode;
+		this.writeDate = writeDate;
+		this.updateDate = updateDate;
+		this.recommandation = recommandation;
+		this.postCode = postCode;
+	}
+
+	
 }
