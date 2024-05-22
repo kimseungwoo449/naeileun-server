6919454a0d269e3cd07596ed97b472a4219e8a83
c-chae-eun-n <ce.hue125@gmail.com>
2024-05-22 10:48:37 +0900

    Update : BoardRequestDto 변수 추가

--- src/main/java/board/model/BoardRequestDto.java
+++ src/main/java/board/model/BoardRequestDto.java
@@ -1,5 +1,13 @@
 package board.model;
 
 public class BoardRequestDto {
+	private int boardCode;
+	private String boardName;
+	private String description;
+	private String title;
+	private String content;
+	private int userCode;
+	private int recommandation;
+	private int postCode;
 
 }
