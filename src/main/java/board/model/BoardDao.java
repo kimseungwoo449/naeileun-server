63e57edec609c5b41d680e5883e925c10b4e82a3
c-chae-eun-n <ce.hue125@gmail.com>
2024-05-22 11:02:36 +0900

    Update : singleton 생성자 추가

--- src/main/java/board/model/BoardDao.java
+++ src/main/java/board/model/BoardDao.java
@@ -2,4 +2,15 @@ package board.model;
 
 public class BoardDao {
 
+	private BoardDao() {
+		
+	}
+	
+	private static BoardDao instance = new BoardDao();
+	
+	public static BoardDao getInstance() {
+		return instance;
+	}
+	
+	
 }
