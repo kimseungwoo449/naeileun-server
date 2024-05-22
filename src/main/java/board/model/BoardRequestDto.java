2e38ca6df13da378e13d0a7b39f93007acf9ed60
c-chae-eun-n <ce.hue125@gmail.com>
2024-05-22 10:50:59 +0900

    Update : 생성자 추가

--- src/main/java/board/model/BoardRequestDto.java
+++ src/main/java/board/model/BoardRequestDto.java
@@ -9,5 +9,38 @@ public class BoardRequestDto {
 	private int userCode;
 	private int recommandation;
 	private int postCode;
+	
+	// 기본 생성자
+	public BoardRequestDto() {
+		super();
+	}
 
+	// 게시판 설명 제되 생성자
+	public BoardRequestDto(int boardCode, String boardName, String title, String content, int userCode,
+			int recommandation, int postCode) {
+		super();
+		this.boardCode = boardCode;
+		this.boardName = boardName;
+		this.title = title;
+		this.content = content;
+		this.userCode = userCode;
+		this.recommandation = recommandation;
+		this.postCode = postCode;
+	}
+
+	// 게시판 설명 포함 셍성자
+	public BoardRequestDto(int boardCode, String boardName, String description, String title, String content,
+			int userCode, int recommandation, int postCode) {
+		super();
+		this.boardCode = boardCode;
+		this.boardName = boardName;
+		this.description = description;
+		this.title = title;
+		this.content = content;
+		this.userCode = userCode;
+		this.recommandation = recommandation;
+		this.postCode = postCode;
+	}
+
+	
 }
