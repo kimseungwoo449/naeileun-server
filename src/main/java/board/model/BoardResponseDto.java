67260282f1d9f0bda52b799f759453b34bf1f572
c-chae-eun-n <ce.hue125@gmail.com>
2024-05-22 10:54:33 +0900

    Update : 변수 추가

--- src/main/java/board/model/BoardResponseDto.java
+++ src/main/java/board/model/BoardResponseDto.java
@@ -1,5 +1,18 @@
 package board.model;
 
+import java.sql.Timestamp;
+
 public class BoardResponseDto {
+	private int boardCode;
+	private String boardName;
+	private String description;
+	private Timestamp createDate;
+	private String title;
+	private String content;
+	private int userCode;
+	private Timestamp writeDate;
+	private Timestamp updateDate;
+	private int recommandation;
+	private int postCode;
 
 }
