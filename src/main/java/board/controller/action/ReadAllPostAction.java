df7c79958e6f19684d8222278450f57a3cb5cbf4
c-chae-eun-n <ce.hue125@gmail.com>
2024-05-22 16:06:45 +0900

    Update : API 생성

--- src/main/java/board/controller/action/ReadAllPostAction.java
+++ src/main/java/board/controller/action/ReadAllPostAction.java
@@ -1,12 +1,19 @@
 package board.controller.action;
 
 import java.io.IOException;
+import java.util.HashMap;
+import java.util.List;
 
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import org.json.JSONArray;
+import org.json.JSONObject;
+
 import board.controller.Action;
+import board.model.BoardDao;
+import board.model.BoardResponseDto;
 
 public class ReadAllPostAction implements Action {
 
@@ -14,6 +21,24 @@ public class ReadAllPostAction implements Action {
 	public void excute(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		System.out.println("읽어오기");
 		
+		BoardDao boardDao = BoardDao.getInstance();
+		
+		List<BoardResponseDto> postList = boardDao.readAllPost();
+		
+		JSONObject resObj = new JSONObject();
+		
+		JSONObject meta = new JSONObject();
+		meta.put("total_count", postList.size());
+		meta.put("pageable_count", 10);
+		
+		JSONArray result = new JSONArray(postList);
+		
+		resObj.put("meta", meta);
+		resObj.put("result", result);
+		System.out.println(resObj);
+		
+		response.setCharacterEncoding("UTF-8");
+		response.setContentType("application/json;charset=UTF-8");
+		response.getWriter().append(resObj.toString());
 	}
-
 }
