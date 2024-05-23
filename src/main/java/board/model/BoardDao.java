ee9a6991684a1c953591d22e56c5020f5cc59aff
c-chae-eun-n <ce.hue125@gmail.com>
2024-05-23 17:21:51 +0900

    Update : readAllBoard & readAllPostByBoardCode 메서드 추가

--- src/main/java/board/model/BoardDao.java
+++ src/main/java/board/model/BoardDao.java
@@ -53,8 +53,78 @@ public class BoardDao {
 				int recommandation = rs.getInt(10);
 				int postCode = rs.getInt(12);
 				
-				BoardResponseDto board = new BoardResponseDto(boardCode, boardName, description, createdDate, title, content, userId, writeDate, updateDate, recommandation, postCode);
-				postList.add(board);
+				BoardResponseDto post = new BoardResponseDto(boardCode, boardName, description, createdDate, title, content, userId, writeDate, updateDate, recommandation, postCode);
+				postList.add(post);
+			}
+		} catch (Exception e) {
+			e.printStackTrace();
+		} finally {
+			DBManager.close(conn, pstmt, rs);
+		}
+		
+		return postList;
+	}
+	
+	public List<BoardResponseDto> readAllBoard(){
+		List<BoardResponseDto> boardList = new ArrayList<BoardResponseDto>();
+		
+		try {
+			conn = DBManager.getConnection();
+			
+			String sql = "SELECT board_code, board_name, description, created_date FROM board_category";
+			
+			pstmt =  conn.prepareStatement(sql);
+			
+			rs = pstmt.executeQuery();
+			
+			while(rs.next()) {
+				int boardCode = rs.getInt(1);
+				String boardName = rs.getString(2);
+				String description = rs.getString(3) == null ? "" : rs.getString(3);
+				Timestamp createdDate = rs.getTimestamp(4);
+				
+				BoardResponseDto board = new BoardResponseDto(boardCode, boardName, description, createdDate);
+				boardList.add(board);
+			}
+		} catch (Exception e) {
+			e.printStackTrace();
+		} finally {
+			DBManager.close(conn, pstmt, rs);
+		}
+		
+		return boardList;
+	}
+	
+	public List<BoardResponseDto> readAllPostByBoardCode(int code){
+		List<BoardResponseDto> postList = new ArrayList<BoardResponseDto>();
+		
+		try {
+			conn = DBManager.getConnection();
+			UserDao userDao = UserDao.getInstance();
+			
+			String sql = "SELECT board_code, board_name, description, created_date, title, content, user_code, write_date, update_date, recommandation, post_code FROM board WHERE board_code=?";
+			
+			pstmt =  conn.prepareStatement(sql);
+			pstmt.setInt(1, code);
+			
+			rs = pstmt.executeQuery();
+			
+			while(rs.next()) {
+				int boardCode = rs.getInt(1);
+				String boardName = rs.getString(2);
+				String description = rs.getString(3) == null ? "" : rs.getString(3);
+				Timestamp createdDate = rs.getTimestamp(4);
+				String title = rs.getString(5);
+				String content = rs.getString(6);
+//				String userId = userDao(rs.getInt(7));
+				String userId = "1234";
+				Timestamp writeDate = rs.getTimestamp(8);
+				Timestamp updateDate = rs.getTimestamp(9);
+				int recommandation = rs.getInt(10);
+				int postCode = rs.getInt(11);
+				
+				BoardResponseDto post = new BoardResponseDto(boardCode, boardName, description, createdDate, title, content, userId, writeDate, updateDate, recommandation, postCode);
+				postList.add(post);
 			}
 		} catch (Exception e) {
 			e.printStackTrace();
