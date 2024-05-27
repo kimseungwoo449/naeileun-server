aa93acb6452064704d94755e8ce25f5c3d2f4f4b
da092511 <da092511@naver.com>
2024-05-27 16:21:14 +0900

    Modify add constructors, getter and setter

--- src/main/java/study/model/groupPost/GroupPostRequestDto.java
+++ src/main/java/study/model/groupPost/GroupPostRequestDto.java
@@ -1,5 +1,87 @@
 package study.model.groupPost;
 
 public class GroupPostRequestDto {
+    String postCode;
+    String userCode;
+    String groupCode;
+    String title;
+    String content;
+    String writeDate;
+    String updateDate;
 
+    public GroupPostRequestDto() {
+    }
+
+    public GroupPostRequestDto(String userCode, String groupCode, String title, String content) {
+        this.userCode = userCode;
+        this.groupCode = groupCode;
+        this.title = title;
+        this.content = content;
+    }
+
+    public GroupPostRequestDto(String postCode, String userCode, String groupCode, String title, String content, String writeDate, String updateDate) {
+        this.postCode = postCode;
+        this.userCode = userCode;
+        this.groupCode = groupCode;
+        this.title = title;
+        this.content = content;
+        this.writeDate = writeDate;
+        this.updateDate = updateDate;
+    }
+
+    public String getPostCode() {
+        return postCode;
+    }
+
+    public void setPostCode(String postCode) {
+        this.postCode = postCode;
+    }
+
+    public String getUserCode() {
+        return userCode;
+    }
+
+    public void setUserCode(String userCode) {
+        this.userCode = userCode;
+    }
+
+    public String getGroupCode() {
+        return groupCode;
+    }
+
+    public void setGroupCode(String groupCode) {
+        this.groupCode = groupCode;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getContent() {
+        return content;
+    }
+
+    public void setContent(String content) {
+        this.content = content;
+    }
+
+    public String getWriteDate() {
+        return writeDate;
+    }
+
+    public void setWriteDate(String writeDate) {
+        this.writeDate = writeDate;
+    }
+
+    public String getUpdateDate() {
+        return updateDate;
+    }
+
+    public void setUpdateDate(String updateDate) {
+        this.updateDate = updateDate;
+    }
 }
