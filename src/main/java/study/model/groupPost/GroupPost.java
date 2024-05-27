8bd03451759f9e89cab0dc030007c19a8ace463b
da092511 <da092511@naver.com>
2024-05-27 16:28:29 +0900

    Modify add constructor and getters

--- src/main/java/study/model/groupPost/GroupPost.java
+++ src/main/java/study/model/groupPost/GroupPost.java
@@ -1,5 +1,43 @@
 package study.model.groupPost;
 
 public class GroupPost {
+    String postCode;
+    String userCode;
+    String groupCode;
+    String title;
+    String content;
+    String updateDate;
 
+    public GroupPost(String postCode, String userCode, String groupCode, String title, String content, String updateDate) {
+        this.postCode = postCode;
+        this.userCode = userCode;
+        this.groupCode = groupCode;
+        this.title = title;
+        this.content = content;
+        this.updateDate = updateDate;
+    }
+
+    public String getPostCode() {
+        return postCode;
+    }
+
+    public String getUserCode() {
+        return userCode;
+    }
+
+    public String getGroupCode() {
+        return groupCode;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public String getContent() {
+        return content;
+    }
+
+    public String getUpdateDate() {
+        return updateDate;
+    }
 }
