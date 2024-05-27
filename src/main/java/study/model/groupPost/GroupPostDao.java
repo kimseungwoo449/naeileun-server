6adec76f3f6cd692ab8edeff03e3fd902d5a48b1
da092511 <da092511@naver.com>
2024-05-27 18:08:51 +0900

    Modify getGroupPostByGroupCode method

--- src/main/java/study/model/groupPost/GroupPostDao.java
+++ src/main/java/study/model/groupPost/GroupPostDao.java
@@ -1,8 +1,12 @@
 package study.model.groupPost;
 
+import utill.DBManager;
+
 import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
 import java.util.List;
 
 public class GroupPostDao {
@@ -20,6 +24,33 @@ public class GroupPostDao {
 
     public List<GroupPostResponseDto> getGroupPostsByGroupCode(String groupCode){
         List<GroupPostResponseDto> list = null;
+
+        try{
+            conn = DBManager.getConnection();
+            String sql = "SELECT post_code,user_code,title,content,update_date FROM group_posts WHERE group_code =?";
+            pstmt = conn.prepareStatement(sql);
+            pstmt.setString(1,groupCode);
+
+            rs = pstmt.executeQuery();
+
+            list = new ArrayList<GroupPostResponseDto>();
+
+            while(rs.next()){
+                String postCode = rs.getString(1);
+                String userCode = rs.getString(2);
+                String title = rs.getString(3);
+                String content = rs.getString(4);
+                String updateDate = rs.getString(5);
+
+                GroupPostResponseDto gp = new GroupPostResponseDto(postCode,groupCode,userCode,title,content,updateDate);
+                list.add(gp);
+            }
+
+        }catch(SQLException e){
+            e.printStackTrace();
+        }finally{
+            DBManager.close(conn,pstmt,rs);
+        }
 
         return list;
     }
