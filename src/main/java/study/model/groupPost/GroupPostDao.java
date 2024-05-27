88d60558cb9f2f45dc4b6ae2620a8b6715387124
da092511 <da092511@naver.com>
2024-05-27 16:21:43 +0900

    Modify add getInstance method

--- src/main/java/study/model/groupPost/GroupPostDao.java
+++ src/main/java/study/model/groupPost/GroupPostDao.java
@@ -1,6 +1,26 @@
 package study.model.groupPost;
 
-public class GroupPostDao {
-	
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.util.List;
 
+public class GroupPostDao {
+    Connection conn;
+    PreparedStatement pstmt;
+    ResultSet rs;
+
+	private GroupPostDao(){}
+
+    private static GroupPostDao instance = new GroupPostDao();
+
+    public static GroupPostDao getInstance() {
+        return instance;
+    }
+
+    public List<GroupPostResponseDto> getGroupPostsByGroupCode(String groupCode){
+        List<GroupPostResponseDto> list = null;
+
+        return list;
+    }
 }
