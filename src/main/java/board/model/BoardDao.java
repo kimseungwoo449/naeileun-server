dbbb92c52cc24f49a9723bc427597275d1258ef4
c-chae-eun-n <ce.hue125@gmail.com>
2024-05-27 18:03:53 +0900

    Update : boardDao readPostByBoardCodeAndPostCode setInt 변경

--- src/main/java/board/model/BoardDao.java
+++ src/main/java/board/model/BoardDao.java
@@ -205,26 +205,27 @@ public class BoardDao {
 		try {
 			conn = DBManager.getConnection();
 			UserDao userDao = UserDao.getInstance();
-			
+
 			String sql = "SELECT title, content, user_code, write_date, update_date, recommandation, post_code, board_code FROM posts WHERE post_code=? AND board_code=?";
-			
+
 			pstmt = conn.prepareStatement(sql);
-			pstmt.setInt(1, boardCodeTemp);
-			pstmt.setInt(2, postCodeTemp);
-			
+			pstmt.setInt(1, postCodeTemp);
+			pstmt.setInt(2, boardCodeTemp);
+
 			rs = pstmt.executeQuery();
-			
+
 			if(rs.next()) {
 				String title = rs.getString(1);
 				String content = rs.getString(2);
-				String userId = userDao.findUserIdByCode(rs.getInt(7)+"");
+				String userId = userDao.findUserIdByCode(rs.getInt(3)+"");
 				Timestamp writeDate = rs.getTimestamp(4);
 				Timestamp updateDate = rs.getTimestamp(5);
 				int recommandation = rs.getInt(6);
 				int postCode = rs.getInt(7);
 				int boardCode = rs.getInt(8);
-				
+
 				post = new BoardResponseDto(title, content, userId, writeDate, updateDate, recommandation, postCode, boardCode);
+				System.out.println("readPostByBoardCodeAndPostCode post : " + post);
 			}
 		} catch (Exception e) {
 			e.printStackTrace();
