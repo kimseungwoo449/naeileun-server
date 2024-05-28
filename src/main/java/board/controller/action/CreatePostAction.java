1d2350ac1e019718d024204af6d8cdcfdbe32b91
c-chae-eun-n <ce.hue125@gmail.com>
2024-05-28 20:37:04 +0900

    Modify : CreatePostAction

---
+++ src/main/java/board/controller/action/CreatePostAction.java
@@ -0,0 +1,39 @@
+package board.controller.action;
+
+import board.controller.Action;
+import org.json.JSONObject;
+import utill.IPAdressManager;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+
+public class CreatePostAction implements Action {
+    @Override
+    public void excute(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        JSONObject resObj = new JSONObject();
+
+        boolean status = true;
+
+        if (!request.getHeader("Authorization").equals(IPAdressManager.ADMIN_KEY)) {
+            status = false;
+        } else {
+            InputStream in = request.getInputStream();
+            BufferedReader br = new BufferedReader(new InputStreamReader(in));
+
+            String data = "";
+            int count = 0;
+
+            while (br.ready()) {
+                data += br.readLine() + "\n";
+                count++;
+            }
+
+            JSONObject reqObj = new JSONObject(data);
+        }
+    }
+}
