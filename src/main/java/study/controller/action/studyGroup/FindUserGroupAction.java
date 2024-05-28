a63c18be3c0e3aed1436db2cf981dfab1569466b
da092511 <da092511@naver.com>
2024-05-28 12:45:55 +0900

    Modify delete unused import

--- src/main/java/study/controller/action/studyGroup/FindUserGroupAction.java
+++ src/main/java/study/controller/action/studyGroup/FindUserGroupAction.java
@@ -7,7 +7,6 @@ import java.util.List;
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-import javax.servlet.http.HttpSession;
 
 import org.json.JSONArray;
 import org.json.JSONObject;
