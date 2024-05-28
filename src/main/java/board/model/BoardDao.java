5908770f77ca65a74425130416205075092d96e4
c-chae-eun-n <ce.hue125@gmail.com>
2024-05-28 18:03:36 +0900

    Update : BoardDao createBoard 메소드 -> createPost 변경 & findPostCodeAndBoardCodeRecently 메소드 추가

--- src/main/java/board/model/BoardDao.java
+++ src/main/java/board/model/BoardDao.java
@@ -26,29 +26,28 @@ public class BoardDao {
 		return instance;
 	}
 	
-	public BoardResponseDto createBoard(BoardRequestDto boardDto) {
+	public BoardResponseDto createPost(String reqTitle, String reqContent, String reqUserId, String reqBoardCode) {
 		try {
 			conn = DBManager.getConnection();
+			UserDao userDao = UserDao.getInstance();
 			
-			String sql = "";
-			
-			if(boardDto.getDescription() != null) {
-				sql = "INSERT INTO board_category(board_name, description) VALUES(?, ?)";
-				
-				pstmt = conn.prepareStatement(sql);
-				
-				pstmt.setString(1, boardDto.getBoardName());
-				pstmt.setString(2, boardDto.getDescription());
-			} else {
-				sql = "INSERT INTO board_category(board_name) VALUES(?)";
+			String sql = "INSERT INTO posts(title, content, user_code, board_code) VALUES(?,?, ?, ?)";
 
-				pstmt = conn.prepareStatement(sql);
-				
-				pstmt.setString(1, boardDto.getBoardName());
-			}
+			pstmt = conn.prepareStatement(sql);
+
+			pstmt.setString(1, reqTitle);
+			pstmt.setString(2, reqContent);
+			pstmt.setInt(3, userDao.findUserCodeById(reqUserId));
+			pstmt.setInt(4, Integer.parseInt(reqBoardCode));
+
 			pstmt.execute();
-			
-			return findBoardByName(boardDto.getBoardName());
+
+			String[] tempCode = findPostCodeAndBoardCodeRecently().split("/");
+
+			int postCode = Integer.parseInt(tempCode[0]);
+			int boardCode = Integer.parseInt(tempCode[1]);
+
+			return readPostByBoardCodeAndPostCode(boardCode, postCode);
 			
 		} catch (Exception e) {
 			e.printStackTrace();
@@ -58,7 +57,33 @@ public class BoardDao {
 		
 		return null;
 	}
-	
+
+	public String findPostCodeAndBoardCodeRecently() {
+		String postAndBoardCode = "";
+
+		try {
+			conn = DBManager.getConnection();
+
+			String sql = "SELECT post_code, board_code FROM posts ORDER BY write_date DESC LIMIT 1";
+
+			pstmt = conn.prepareStatement(sql);
+			rs = pstmt.executeQuery();
+
+			if (rs.next()) {
+				int postCode = rs.getInt("post_code");
+				int boardCode = rs.getInt("board_code");
+
+				postAndBoardCode = postCode + "/" + boardCode;
+			}
+		} catch (Exception e) {
+			e.printStackTrace();
+		} finally {
+			DBManager.close(conn, pstmt, rs);
+		}
+
+		return postAndBoardCode;
+	}
+
 	public BoardResponseDto findBoardByName(String name) {
 		BoardResponseDto board = null;
 		
