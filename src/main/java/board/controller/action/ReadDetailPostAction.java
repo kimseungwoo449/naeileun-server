fb414491cf8592b26df2fb0251a20e118cf9b616
kimseungwoo449 <ehdanf449@naver.com>
2024-05-28 14:31:16 +0900

    reset ReadDetailPostAction

--- src/main/java/board/controller/action/ReadDetailPostAction.java
+++ src/main/java/board/controller/action/ReadDetailPostAction.java
@@ -15,40 +15,40 @@ import utill.KeyManager;
 
 public class ReadDetailPostAction implements Action {
 
-	private int boardCode;
-	private int postCode;
+    private int boardCode;
+    private int postCode;
 
-	public ReadDetailPostAction(int boardCode, int postCode) {
-		this.boardCode = boardCode;
-		this.postCode = postCode;
-	}
+    public ReadDetailPostAction(int boardCode, int postCode) {
+        this.boardCode = boardCode;
+        this.postCode = postCode;
+    }
 
-	@Override
-	public void excute(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		JSONObject resObj = new JSONObject();
+    @Override
+    public void excute(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        JSONObject resObj = new JSONObject();
 
 		if (!request.getHeader("Authorization").equals(KeyManager.ADMIN_KEY)) {
 			boolean status = false;
 			String message = "Board is blocked.";
 
-			resObj.put("status", status);
-			resObj.put("message", message);
-		} else {
-			System.out.println("상세 게시글 가져오기");
+            resObj.put("status", status);
+            resObj.put("message", message);
+        } else {
+            System.out.println("상세 게시글 가져오기");
 
-			BoardDao boardDao = BoardDao.getInstance();
+            BoardDao boardDao = BoardDao.getInstance();
 
-			BoardResponseDto post = boardDao.readPostByBoardCodeAndPostCode(boardCode, postCode);
-			System.out.println("title : "+post.getTitle());
+            BoardResponseDto post = boardDao.readPostByBoardCodeAndPostCode(boardCode, postCode);
+            System.out.println("title : "+post.getTitle());
 
-			resObj.put("result", new JSONObject(post));
-			System.out.println(resObj);
+            resObj.put("result", new JSONObject(post));
+            System.out.println(resObj);
 
-		}
+        }
 
-		response.setCharacterEncoding("UTF-8");
-		response.setContentType("application/json;charset=UTF-8");
-		response.getWriter().append(resObj.toString());
-	}
+        response.setCharacterEncoding("UTF-8");
+        response.setContentType("application/json;charset=UTF-8");
+        response.getWriter().append(resObj.toString());
+    }
 
 }
