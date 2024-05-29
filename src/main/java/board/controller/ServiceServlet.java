8452dbfe3e434f7ba03ac85cd3df1f286afa6a75
c-chae-eun-n <ce.hue125@gmail.com>
2024-05-29 18:41:59 +0900

    Update : @MultipartConfig 추가

--- src/main/java/board/controller/ServiceServlet.java
+++ src/main/java/board/controller/ServiceServlet.java
@@ -2,11 +2,12 @@ package board.controller;
 
 import java.io.IOException;
 import javax.servlet.ServletException;
+import javax.servlet.annotation.MultipartConfig;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-
+@MultipartConfig
 public class ServiceServlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
 	
