af2a240528333c6ab0306b1ca48e5282d3e87974
c-chae-eun-n <ce.hue125@gmail.com>
2024-05-30 18:31:30 +0900

    Modify : UpdateRecommendationAction

---
+++ src/main/java/board/controller/action/UpdateRecommendationAction.java
@@ -0,0 +1,15 @@
+package board.controller.action;
+
+import board.controller.Action;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+public class UpdateRecommendationAction implements Action {
+    @Override
+    public void excute(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
+    }
+}
