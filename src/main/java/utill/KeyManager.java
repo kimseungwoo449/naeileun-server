c56db60966d0da17c964f1ff0457aa77209f46b1
kimseungwoo449 <ehdanf449@naver.com>
2024-05-30 10:01:57 +0900

    gitignore 재 적용

---
+++ src/main/java/utill/KeyManager.java
@@ -0,0 +1,31 @@
+package utill;
+
+import javax.naming.Context;
+import javax.naming.InitialContext;
+
+public class KeyManager {
+    public static String getAdminKey() {
+        String adminKey ="";
+        try {
+            Context initContext = new InitialContext();
+            Context envContext = (Context) initContext.lookup("java:comp/env");
+            adminKey = (String) envContext.lookup("ADMIN_KEY");
+
+        }catch (Exception e){
+            e.printStackTrace();
+        }
+        return adminKey;
+    }
+    public static String getUploadcareKey() {
+        String key ="";
+        try {
+            Context initContext = new InitialContext();
+            Context envContext = (Context) initContext.lookup("java:comp/env");
+            key = (String) envContext.lookup("UPLOADCARE_KEY");
+
+        }catch (Exception e){
+            e.printStackTrace();
+        }
+        return key;
+    }
+}
