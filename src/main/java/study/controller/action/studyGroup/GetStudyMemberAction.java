a039fb17f1ae6acd42d02c797336a2bb10a85e82
da092511 <da092511@naver.com>
2024-05-31 12:45:04 +0900

    Create GetStudyMemberAction

---
+++ src/main/java/study/controller/action/studyGroup/GetStudyMemberAction.java
@@ -0,0 +1,73 @@
+package study.controller.action.studyGroup;
+
+import org.json.JSONObject;
+import study.controller.Action;
+import study.model.groupMember.GroupMemberDao;
+import study.model.groupMember.GroupMemberRequestDto;
+import study.model.groupMember.GroupMemberResponseDto;
+import study.model.studyGroup.StudyGroupDao;
+import user.model.UserDao;
+import utill.KeyManager;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.util.List;
+
+public class GetStudyMemberAction implements Action {
+    @Override
+    public void excute(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        request.setCharacterEncoding("UTF-8");
+
+        JSONObject obj = new JSONObject();
+        boolean state = false;
+        String message = null;
+        String groupCode = null;
+        if (!request.getHeader("Authorization").equals(KeyManager.getAdminKey())) {
+            message = "admin key is not correct";
+        } else {
+            InputStream in = request.getInputStream();
+            BufferedReader br = new BufferedReader(new InputStreamReader(in));
+
+            String data = "";
+
+            while (br.ready()) {
+                data = br.readLine();
+            }
+
+            JSONObject reqObj = new JSONObject(data);
+            System.out.println(reqObj);
+
+            groupCode = reqObj.getString("group_code");
+            System.out.println("groupCode: " + groupCode);
+            //String userId = reqObj.getString("user_id");
+            //UserDoa userDao = UserDao.getInstance();
+            //int userCode = userDao.findUserCodeById(userId);
+            String userCode = "2";
+
+            GroupMemberRequestDto gmReqDto = new GroupMemberRequestDto(groupCode, userCode);
+            GroupMemberDao gmDao = GroupMemberDao.getInstance();
+            List<GroupMemberResponseDto> list = gmDao.getStudyMembers(gmReqDto);
+
+            boolean isValid = list.size() == 0 ? false : true;
+            state = isValid;
+            if(!isValid) {
+                message = "Find Group Member failed.";
+            }else{
+                message = "Find Group Member success.";
+            }
+        }
+
+        obj.put("state", state);
+        obj.put("message", message);
+        obj.put("groupCode",groupCode);
+
+        response.setCharacterEncoding("UTF-8");
+        response.setContentType("application/json;charset=UTF-8");
+        response.getWriter().append(obj.toString());
+    }
+}
