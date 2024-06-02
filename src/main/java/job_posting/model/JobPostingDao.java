4347a727aae5cbf8549ed36a5e4f5296940c0ba7
phanik0 <gacigogi3000@daumm.net>
2024-06-02 13:05:38 +0900

    add jobPostingCreate&Read

--- src/main/java/job_posting/model/JobPostingDao.java
+++ src/main/java/job_posting/model/JobPostingDao.java
@@ -24,47 +24,12 @@ public class JobPostingDao {
     public static JobPostingDao getInstance() {
         return instance;
     }
-    public List<JobPostingResponseDto> findMyJobPostingAll(String userId) {
 
-        List<JobPostingResponseDto> list = new ArrayList<JobPostingResponseDto>();
-
-        try {
-            conn = DBManager.getConnection();
-
-            // 쿼리할 준비
-            String sql = "SELECT posting_id, user_code , company_name,job_title, application_start, application_end, job_description , status FROM job_posting WHERE user_id=?";
-            pstmt = conn.prepareStatement(sql);
-            pstmt.setString(1, userId);
-            // 쿼리 실행
-            rs = pstmt.executeQuery();
-
-            // 튜플 읽기
-            while (rs.next()) {
-                // database의 column index는 1부터 시작!
-                int postingId = rs.getInt(1);
-                int userCode = rs.getInt(2);
-                String companyName = rs.getString(3);
-                String job_title = rs.getString(2);
-                Date applicationStart = rs.getDate(4);
-                Date applicationEnd = rs.getDate(5);
-                String job_description = rs.getString(6);
-                String status = rs.getString(7);
-
-                JobPostingResponseDto jobPost = new JobPostingResponseDto(postingId, userCode, companyName, job_title, applicationStart, applicationEnd, job_description,status);
-                list.add(jobPost);
-            }
-        } catch (SQLException e) {
-            e.printStackTrace();
-        }finally {
-            DBManager.close(conn, pstmt,rs);
-        }
-        return list;
-    }
     public JobPostingResponseDto createJobPosting(JobPostingRequestDto jobPost){
         JobPostingResponseDto dto = null;
         try {
             conn = DBManager.getConnection();
-            String sql = "INSERT INTO job-posting(user_code , company_name,job_title, application_start, application_end, job_description) VALUES(?,?,?,?,?,?)";;
+            String sql = "INSERT INTO job_posting(user_code , company_name,job_title, application_start, application_end, job_description) VALUES(?,?,?,?,?,?)";;
             pstmt = conn.prepareStatement(sql);
             pstmt.setInt(1,jobPost.getUserCode());
             pstmt.setString(2,jobPost.getCompanyName());
@@ -83,4 +48,41 @@ public class JobPostingDao {
         return dto;
     }
 
+    public List<JobPostingResponseDto> findMyJobPostingAll(int userCode) {
+
+        List<JobPostingResponseDto> list = new ArrayList<JobPostingResponseDto>();
+
+        try {
+            conn = DBManager.getConnection();
+
+            // 쿼리할 준비
+            String sql = "SELECT posting_id , company_name,job_title, application_start, application_end, job_description , status FROM job_posting WHERE user_code=?";
+            pstmt = conn.prepareStatement(sql);
+            pstmt.setInt(1, userCode);
+            // 쿼리 실행
+            rs = pstmt.executeQuery();
+            System.out.println("1");
+            // 튜플 읽기
+            while (rs.next()) {
+                // database의 column index는 1부터 시작!
+                int postingId = rs.getInt(1);
+                String companyName = rs.getString(2);
+                String job_title = rs.getString(3);
+                Date applicationStart = rs.getDate(4);
+                Date applicationEnd = rs.getDate(5);
+                String job_description = rs.getString(6);
+                String status = rs.getString(7);
+
+                JobPostingResponseDto jobPost = new JobPostingResponseDto(postingId, userCode, companyName, job_title, applicationStart, applicationEnd, job_description,status);
+                list.add(jobPost);
+                System.out.println("내자료 찾기성공");
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+            System.out.println("내자료 찾기실패");
+        }finally {
+            DBManager.close(conn, pstmt,rs);
+        }
+        return list;
+    }
 }
