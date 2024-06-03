6bb007010fc6b2336a9b538892da06088caa9048
kimseungwoo449 <ehdanf449@naver.com>
2024-06-03 18:30:58 +0900

    modify : serviceservlet

--- src/main/java/message/controller/ServiceServlet.java
+++ src/main/java/message/controller/ServiceServlet.java
@@ -39,4 +39,9 @@ public class ServiceServlet extends HttpServlet {
 	protected void doPut(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		doGet(request, response);
 	}
+
+	@Override
+	protected void doDelete(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		doGet(request, response);
+	}
 }
