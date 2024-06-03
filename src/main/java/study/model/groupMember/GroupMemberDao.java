2135e0b8f5455fe87cbcf33181f315acbb7e40a3
da092511 <da092511@naver.com>
2024-06-04 08:18:14 +0900

    Add methods (deleteGroupMember,joinGroupMember,deleteGroupMemberByMemberCode)

--- src/main/java/study/model/groupMember/GroupMemberDao.java
+++ src/main/java/study/model/groupMember/GroupMemberDao.java
@@ -139,7 +139,7 @@ public class GroupMemberDao {
 			 conn = DBManager.getConnection();
 			 String sql = "SELECT gm.user_code, gm.member_code,"
 			 +"(SELECT id From users u WHERE u.user_code = gm.user_code)" +
-					 "FROM group_member gm WHERE group_code = ? AND accessed = true";
+					 "FROM group_member gm WHERE group_code = ? ";
 
 			 String groupCode = groupMemberRequestDto.getGroupCode();
 			 String adminCode = groupMemberRequestDto.getUserCode();
@@ -204,6 +204,73 @@ public class GroupMemberDao {
 		}
 
 		return list;
+	}
+
+	public boolean deleteGroupMember(GroupMemberRequestDto groupMemberRequestDto){
+		boolean isValid = false;
+		try{
+			System.out.println("here : "+isValid);
+			conn = DBManager.getConnection();
+			String sql = "DELETE FROM group_member WHERE group_code=? AND user_code=?";
+			pstmt = conn.prepareStatement(sql);
+
+			String groupCode = groupMemberRequestDto.getGroupCode();
+			String userCode = groupMemberRequestDto.getUserCode();
+
+			pstmt.setString(1,groupCode);
+			pstmt.setString(2,userCode);
+			pstmt.execute();
+			isValid = true;
+
+			System.out.println("isValid : "+isValid);
+		}catch(SQLException e){
+			e.printStackTrace();
+		}finally{
+			DBManager.close(conn, pstmt);
+		}
+		return isValid;
+	}
+
+	public boolean joinGroupMember(GroupMemberRequestDto groupMemberRequestDto){
+		boolean isValid = false;
+		try{
+			conn = DBManager.getConnection();
+			String sql = "INSERT INTO group_member (group_code,user_code) VALUES (?,?)";
+			pstmt = conn.prepareStatement(sql);
+			String groupCode = groupMemberRequestDto.getGroupCode();
+			String userCode = groupMemberRequestDto.getUserCode();
+
+			pstmt.setString(1,groupCode);
+			pstmt.setString(2,userCode);
+			pstmt.execute();
+			isValid = true;
+		}catch(SQLException e){
+			e.printStackTrace();
+		}finally{
+			DBManager.close(conn, pstmt);
+		}
+		return isValid;
+	}
+
+	public boolean deleteGroupMemberByMemberCode(GroupMemberRequestDto groupMemberRequestDto){
+		boolean isValid = false;
+
+		try{
+			String memberCode = groupMemberRequestDto.getMemberCode();
+
+			conn = DBManager.getConnection();
+			String sql = "DELETE FROM group_member WHERE member_code=?";
+			pstmt = conn.prepareStatement(sql);
+			pstmt.setString(1,memberCode);
+
+			pstmt.execute();
+			isValid = true;
+		}catch(SQLException e){
+			e.printStackTrace();
+		}finally{
+			DBManager.close(conn, pstmt);
+		}
+		return isValid;
 	}
 	
 }
