31a320bca3ad71d88af34943399613358e312fc3
da092511 <da092511@naver.com>
2024-06-04 02:17:37 +0900

    Modify getAction method

--- src/main/java/study/controller/ActionFactory.java
+++ src/main/java/study/controller/ActionFactory.java
@@ -1,6 +1,5 @@
 package study.controller;
 
-import study.controller.Action;
 import study.controller.action.studyGroup.*;
 
 public class ActionFactory{
@@ -18,9 +17,7 @@ public class ActionFactory{
 	public Action getAction(String command) {
 		Action action = null;
 		if(command.equals("/myGroup")) {
-			action = new FindUserGroupAction();
-		}else if(command.equals("/popularGroup")){
-			action = new FindPopularStudyGroupAction();
+			action = new GetStudyGroupAction();
 		}else if(command.equals("/board")){
 			action = new FindStudyBoardAction();
 		}else if(command.equals("/delete")){
@@ -29,6 +26,8 @@ public class ActionFactory{
 			action = new CreateStudyAction();
 		}else if(command.equals("/getMember")){
 			action = new GetStudyMemberAction();
+		}else if(command.equals("/getAwaiter")){
+			action = new GetStudyAwaiterAction();
 		}
 		
 		return action;
