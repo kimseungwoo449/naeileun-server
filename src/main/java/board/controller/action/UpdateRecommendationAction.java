b2a0b881cc0fbc85016e4bd18412a55be42b35e8
c-chae-eun-n <ce.hue125@gmail.com>
2024-06-04 18:24:41 +0900

    Modify : UpdateRecommendationAction

--- src/main/java/board/controller/action/UpdateRecommendationAction.java
+++ src/main/java/board/controller/action/UpdateRecommendationAction.java
@@ -1,6 +1,9 @@
 package board.controller.action;
 
 import board.controller.Action;
+import board.model.BoardDao;
+import org.json.JSONObject;
+import utill.KeyManager;
 
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
@@ -10,6 +13,37 @@ import java.io.IOException;
 public class UpdateRecommendationAction implements Action {
     @Override
     public void excute(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        JSONObject resObj = new JSONObject();
 
+        System.out.println("추천수 올리기");
+
+        boolean status = true;
+        String message = "Recommendation is Success.";
+        if (!request.getHeader("Authorization").equals(KeyManager.getAdminKey())) {
+            status = false;
+            message = "Recommendation is blocked.";
+
+        } else {
+            int postCode = Integer.parseInt(request.getParameter("post_code"));
+
+            BoardDao boardDao = BoardDao.getInstance();
+
+            boolean isUpdate = boardDao.updateRecommendationByPostCode(postCode);
+
+            if(!isUpdate) {
+                status = false;
+                message = "Recommendation is blocked.";
+            }
+
+            System.out.println("isUpdate : " + isUpdate);
+
+        }
+
+        resObj.put("status", status);
+        resObj.put("message", message);
+
+        response.setCharacterEncoding("UTF-8");
+        response.setContentType("application/json;charset=UTF-8");
+        response.getWriter().append(resObj.toString());
     }
 }
