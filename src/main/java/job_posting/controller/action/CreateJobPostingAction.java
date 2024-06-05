79fe9c0f316b3c56d3909ef3214f5cba2a7206f6
phanik0 <gacigogi3000@naver.com>
2024-06-05 18:36:49 +0900

    add jobposting crud

--- src/main/java/job_posting/controller/action/CreateJobPostingAction.java
+++ src/main/java/job_posting/controller/action/CreateJobPostingAction.java
@@ -7,6 +7,7 @@ import job_posting.model.JobPostingDao;
 import job_posting.model.JobPostingRequestDto;
 import job_posting.model.JobPostingResponseDto;
 import org.json.JSONObject;
+import utill.DBManager;
 
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
@@ -15,12 +16,12 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
+import java.sql.SQLException;
 
 public class CreateJobPostingAction implements Action {
     @Override
     public void excute(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-        System.out.println("ok");
-        System.out.println("10");
+
         InputStream in = request.getInputStream();
         BufferedReader br = new BufferedReader(new InputStreamReader(in));
 
@@ -28,7 +29,7 @@ public class CreateJobPostingAction implements Action {
         while(br.ready()){
             data += br.readLine() + "\n";
         }
-        System.out.println("data: " + data);
+
         Gson gson = new GsonBuilder()
                 .setDateFormat("yyyy-MM-dd")
                 .create();
@@ -36,23 +37,23 @@ public class CreateJobPostingAction implements Action {
         JobPostingRequestDto jobPost = gson.fromJson(data, JobPostingRequestDto.class);
 
         // Create user
-        JobPostingDao userDao = JobPostingDao.getInstance();
-        JobPostingResponseDto userDto = userDao.createJobPosting(jobPost);
+        JobPostingDao jobDao = JobPostingDao.getInstance();
+        JobPostingResponseDto jobDto = jobDao.createJobPosting(jobPost);
 
         // Prepare response JSON object
         JSONObject resObj = new JSONObject();
-        int status = (userDto != null) ? 200 : 400;
-        String message = (userDto != null) ? "User registration is success." : "User registration is failed.";
-        System.out.println("status: " + status);
-        System.out.println("message: " + message);
+        int status = (jobDto != null) ? 200 : 400;
+        String message = (jobDto != null) ? "JobPosting registration is success." : "JobPosting registration is failed.";
+
         resObj.put("status", status);
         resObj.put("message", message);
 
-        // Send response
+
         response.setCharacterEncoding("UTF-8");
         response.setContentType("application/json;charset=UTF-8");
         response.getWriter().append(resObj.toString());
 
-        System.out.println(message);
+
     }
+
 }
--- src/main/java/job_posting/controller/action/DeleteJobPostingAction.java
+++ src/main/java/job_posting/controller/action/DeleteJobPostingAction.java
@@ -1,15 +1,44 @@
 package job_posting.controller.action;
 
+import com.google.gson.Gson;
 import job_posting.controller.Action;
+import job_posting.model.JobPostingDao;
+import job_posting.model.JobPostingRequestDto;
+import job_posting.model.JobPostingResponseDto;
+import org.json.JSONObject;
 
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import java.io.BufferedReader;
 import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
 
 public class DeleteJobPostingAction implements Action {
     @Override
     public void excute(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        InputStream in = request.getInputStream();
+        BufferedReader br = new BufferedReader(new InputStreamReader(in));
+        String data = "";
+        while(br.ready()){
+            data += br.readLine() + "\n";
+        }
+        Gson gson = new Gson();
+        JobPostingRequestDto jobPost = gson.fromJson(data, JobPostingRequestDto.class);
+        JobPostingDao jobDao = JobPostingDao.getInstance();
+        JSONObject resObj = new JSONObject();
+        int status = jobDao.deleteJobPosting(jobPost) ? 200 : 400;
+        String message = status == 200 ? "JobPosting delete is success." : "JobPosting delete is failed.";
+        resObj.put("status", status);
+        resObj.put("message", message);
+
+
+        response.setCharacterEncoding("UTF-8");
+        response.setContentType("application/json;charset=UTF-8");
+        response.getWriter().append(resObj.toString());
+
 
     }
+
 }
