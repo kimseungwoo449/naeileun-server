5429710ddc1fd2c520bbb77931c2d6132a00bb31
da092511 <da092511@naver.com>
2024-06-05 17:32:46 +0900

    Add getStudyByGroupCode method

--- src/main/java/study/model/studyGroup/StudyGroupDao.java
+++ src/main/java/study/model/studyGroup/StudyGroupDao.java
@@ -77,7 +77,7 @@ public class StudyGroupDao {
 	public StudyGroupResponseDto getStudyByGroupCode(String groupCode){
 
 		StudyGroupResponseDto study = null;
-		
+
 		if(groupCode == null)
 			return study;
 		
@@ -109,6 +109,42 @@ public class StudyGroupDao {
 			DBManager.close(conn, pstmt, rs);
 		}
 		
+		return study;
+	}
+
+	public StudyGroupResponseDto getStudyByGroupCode(StudyGroupRequestDto sgReqDto){
+		StudyGroupResponseDto study = null;
+
+		try {
+			String groupCode = sgReqDto.getGroupCode();
+
+			conn = DBManager.getConnection();
+			String sql = "SELECT name,decription, admin_code, is_public,auto_member_access FROM study_group WHERE group_code = ?";
+			pstmt = conn.prepareStatement(sql);
+
+			pstmt.setString(1,groupCode);
+
+			rs = pstmt.executeQuery();
+
+			if(rs.next()) {
+				String name = rs.getString(1);
+				String decription =  rs.getString(2);
+				String adminCode =  rs.getString(3);
+				String isPublic =  rs.getString(4).equals("0") ? "false" : "true";
+				String autoMemberAccess = rs.getString(5).equals("0") ? "false" : "true";
+				if(decription == null)
+					study = new StudyGroupResponseDto(groupCode, name,adminCode,isPublic,autoMemberAccess);
+				else
+					study = new StudyGroupResponseDto(groupCode, name,decription,adminCode,isPublic,autoMemberAccess);
+			}
+
+			System.out.println("DB 연동 성공");
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}finally {
+			DBManager.close(conn, pstmt, rs);
+		}
+
 		return study;
 	}
 
@@ -160,10 +196,16 @@ public class StudyGroupDao {
 		return isValid;
 	}
 
-	public boolean createStudy(String name, String adminCode, String decription, boolean isPublic, boolean autoMemberAccess){
+	public boolean createStudy(StudyGroupRequestDto studyGroupRequestDto){
 		boolean isValid = false;
 
 		try{
+			String name = studyGroupRequestDto.getName();
+			String adminCode = studyGroupRequestDto.getAdminCode();
+			String decription = studyGroupRequestDto.getDecription();
+			boolean isPublic = Boolean.parseBoolean(studyGroupRequestDto.getIsPublic());
+			boolean autoMemberAccess = Boolean.parseBoolean(studyGroupRequestDto.getAutoMemberAccess());
+
 			conn = DBManager.getConnection();
 			String sql = "INSERT INTO study_group (name,admin_code,decription,is_public,auto_member_access) " +
 					"VALUES (?,?,?,?,?)";
@@ -188,15 +230,21 @@ public class StudyGroupDao {
 		return isValid;
 	}
 
-	public String createStudyAndGetGroupCode(String name, String adminCode, String decription, boolean isPublic, boolean autoMemberAccess){
+	public String createStudyAndGetGroupCode(StudyGroupRequestDto studyGroupRequestDto){
 		String code = null;
 
-		boolean isValid = createStudy(name,adminCode,decription,isPublic,autoMemberAccess);
+		boolean isValid = createStudy(studyGroupRequestDto);
 
 		if(!isValid)
 			return code;
 
 		try{
+			String name = studyGroupRequestDto.getName();
+			String adminCode = studyGroupRequestDto.getAdminCode();
+			String decription = studyGroupRequestDto.getDecription();
+			boolean isPublic = Boolean.parseBoolean(studyGroupRequestDto.getIsPublic());
+			boolean autoMemberAccess = Boolean.parseBoolean(studyGroupRequestDto.getAutoMemberAccess());
+
 			conn = DBManager.getConnection();
 			if(decription == null){
 				String sql = "SELECT group_code FROM study_group WHERE name = ? AND admin_code =? AND decription IS NULL AND is_public =? AND auto_member_access =?";
