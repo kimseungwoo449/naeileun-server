a317cd18d27ffb0f40b8deb0e1923d1387b47379
da092511 <da092511@naver.com>
2024-06-06 18:46:16 +0900

    Modify : GroupMemberDao 사용하기

--- src/main/java/study/controller/action/studyGroup/GetStudyGroupAction.java
+++ src/main/java/study/controller/action/studyGroup/GetStudyGroupAction.java
@@ -14,6 +14,7 @@ import org.json.JSONArray;
 import org.json.JSONObject;
 
 import study.controller.Action;
+import study.model.groupMember.GroupMemberDao;
 import study.model.groupMember.GroupMemberRequestDto;
 import utill.KeyManager;
 import study.model.studyGroup.StudyGroupDao;
@@ -44,7 +45,8 @@ public class GetStudyGroupAction implements Action{
 			List<StudyGroupResponseDto> list = sgDao.getUserStudyByUserCode(gmDto);
 			JSONArray myStudy = new JSONArray(list);
 
-			int count = sgDao.getMyStudyCount(gmDto);
+			GroupMemberDao gmDao = GroupMemberDao.getInstance();
+			int count = gmDao.getMyStudyCount(gmDto);
 			int totalCount = sgDao.getTotalStudyCount();
 
 			List<StudyGroupResponseDto> popularStudy = sgDao.getPopularStudy();
